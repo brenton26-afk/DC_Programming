90b3ba1f7146101e48a2b6d230fddfd22d2476db
brenton26-afk <77509555+brenton26-afk@users.noreply.github.com>
2023-01-19 09:17:05 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,114 +1,59 @@
+let xp = 200;
+let yp = 200;
+
 function setup() {
   createCanvas(400, 400);
-}
-
-
-let face
-let eye
-let neck
-let arm
-let middle
-let outline
-
-
-function draw() {
-   if(mouseX > 200){
-    background("darkorange")
-    bad()
-  }else{
-    background("yellow")
-    good()
-  }
-  //console.log(mouseX);
-  //console.log(mouseY);
-  circle(mouseX, mouseY, 10)
-  //260 75
+  background(220);
   
-
-  head()
-  eyes()
-  body()
   
- 
-  fill("green")
-  line(250, 75, 275, 50)
-  circle(275, 50, 15)
-  line(150, 75, 125, 50)
-  circle(125, 50, 15)
-
-  arms()
+  strokeWeight(5)
+  fill("white");
+  circle(xp - 97, yp - 50, 123);
   
-  angleMode(DEGREES)
-  noFill()
-  strokeWeight(10)
-  stroke(hand)
-  translate(335, 313)
-  //square(0,0, 25)
-  rotate(115)
-  angleMode(RADIANS)
-  arc(0, 0, 50, 50, 0, PI + HALF_PI);
-  //330, 300
-    rotate(1)
-  arc(210, 5, 50, 50, -0.5, PI + QUARTER_PI);
-  stroke("black")
   
-
+  stroke("white")
+  fill("red");
+  circle(xp + 150, yp - 100, 300);
+  
+  
+  fill("red");
+  circle(xp - 5, yp + 135, 250);
+  
+  
+  fill("darkred");
+  circle(xp - 60, yp - 50, 50);
+  
+  
+  fill("darkred");
+  circle(xp + 35, yp - 75, 111);
+  
+  fill(220)
+  triangle(150, 150, 350, 200, 300, 300);
+  
+  fill("darkorange");
+  circle(xp + 100, yp + 25, 83);
+  
+  
+  fill("darkred");
+  circle(xp - 75, yp + 100, 80);
+  
+  
+  fill("orange");
+  circle(xp + 15, yp - 25, 20);
+  arc(250, 135, 80, 80, 0, PI + HALF_PI + QUARTER_PI);
+  arc(275, 250, 80, 80, 3, PI + PI + HALF_PI);
+  
+  fill("yellow");
+  circle(xp - 45, yp + 45, 83);
+  
+  
+  fill("yellow");
+  circle(xp + 25, yp - 85, 50);
+  
+  
+  fill("orange");
+  circle(xp - 75, yp + 45, 43);
+  
   
   
 }
-
-
-
-
-function eyes(){
-  fill(eye)
-  circle(225, 100, 30)
-  circle(175, 100, 30)
-  
-}
-
-function head(){
-  fill(face)
-  square(150, 75, 100)
-  fill(neck)
-  rect(175, 175, 50, 50)
-  
-}
-
-function body(){
-  fill(middle)
-  rect(125 , 200, 150, 200)
-}
-
-
-function arms(){
-  //275
-  //200
-  fill(arm)
-  
-  angleMode(DEGREES)
-  rotate(-5)
-  rect(257, 223, 30, 125)
-  rotate(10)
-  rect(112, 188, 30, 125)
-}
-
-
-function bad(){
-  eye = "red"
-  face = "black"
-  neck = "gray"
-  arm = "darkred"
-  hand = "red"
-  middle = "gray"
-}
-
-function good(){
-  eye = "green"
-  face = "lightgreen"
-  arm = "turquoise"
-  neck = "green"
-  hand = "blue"
-  middle = "lightblue"
-}
-
