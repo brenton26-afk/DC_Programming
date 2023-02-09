81416d6220c07fb84fd8d79dad8144abe2137b08
brenton26-afk <77509555+brenton26-afk@users.noreply.github.com>
2023-02-09 09:28:28 -0500

    Add files via upload

--- projectGame/main.js
+++ projectGame/main.js
@@ -8,19 +8,30 @@ let yp = [];
 let showImg = false;
 let afterImg = false;
 
+let cRed = 0;
+let cBlue = 0;
+let cGreen = 0;
+
+
 function setup() {
   createCanvas(800, 800);
 }
 
 function draw() {
   background(220);
-  fill("red")
+  
+  let value = mouseY;
+  cRed = map()
   
   
+  fill(cRed, cBlue, cGreen)
+  
+  //shows the "cursor" as you move the mouse across the canvas
   if(showImg == true){
     circle(mouseX, mouseY, cSize)
   }
   
+  //shows the path of the cursor during the mouse being dragged
   if(afterImg == true){
     
     for(let i = 0; i < xp.length; i++){
